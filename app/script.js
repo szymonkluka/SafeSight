a3272e3569b7bc6aa8be5eddbca552f09229dd94
Szymon Kluka <szymonkluka@onet.pl>
2023-03-23 20:28:46 +0100

    SafeSight

--- app/script.js
+++ app/script.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
 
-const App = () => {
-  const status2 = {
-    status: 'work',
-    status: 'rest',
-    status: 'off',
-  }
+const statuses = {
+  work: 'work',
+  off: 'off',
+  rest: 'rest'
+}
 
-  const [status, setStatus] = useState('status2.off');
+const App = () => {
+  const [status, setStatus] = useState(statuses.off);
   const [time, setTime] = useState(0);
   const [timer, setTimer] = useState(null);
 
@@ -19,7 +19,7 @@ const App = () => {
   }
 
   const startTimer = () => {
-    setStatus(status2.work);
+    setStatus(statuses.work);
     setTime(1200);
     setTimer(
       setInterval(() => {
@@ -29,7 +29,7 @@ const App = () => {
   };
 
   const stopTimer = () => {
-    setStatus('status2.off')
+    setStatus(statuses.off)
     setTime(0)
     clearInterval(timer)
   }
@@ -39,21 +39,19 @@ const App = () => {
     audio.play();
   }
 
-  if (time < 0) {
+  if (time === 0) {
 
-    if (status === status2.work) {
-      setStatus('status2.rest');
+    if (status === statuses.work) {
+      setStatus(statuses.rest);
       setTime(20);
       playBell();
     }
-    if (status === 'status2.rest') {
-      setStatus(status2.work)
+    if (status === statuses.rest) {
+      setStatus(statuses.work)
       setTime(1200)
       playBell();
     }
   }
-
-
 
   const windowClose = () => {
     close();
@@ -62,21 +60,21 @@ const App = () => {
   return (
     <div>
       <h1>Protect your eyes</h1>
-      {status === 'status2.off' && (
+      {status === statuses.off && (
         <div>
           <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
           <p>This app will help you track your time and inform you when it's time to rest.</p>
         </div>
       )}
-      {status === status2.work && (<img src="./images/work.png" />)}
-      {status === 'status2.rest' && (<img src="./images/rest.png" />)}
-      {status !== 'status2.off' && (
+      {status === statuses.work && (<img src="./images/work.png" />)}
+      {status === statuses.rest && (<img src="./images/rest.png" />)}
+      {status !== statuses.off && (
         <div className="timer">
           {formatTime(time)}
         </div>
       )}
-      {status === 'status2.off' && (<button className="btn" onClick={startTimer}>Start</button>)}
-      {status !== 'status2.off' && (<button className="btn" onClick={stopTimer}>Stop</button>)}
+      {status === statuses.off && (<button className="btn" onClick={startTimer}>Start</button>)}
+      {status !== statuses.off && (<button className="btn" onClick={stopTimer}>Stop</button>)}
       <button className="btn btn-close" onClick={windowClose}>X</button>
     </div>
   );
